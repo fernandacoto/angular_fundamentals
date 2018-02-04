b0287fb8a8e9a3350944f8caec68a2f07cb323ba
Fernanda Coto <fermarioro@gmail.com>
2018-02-03 18:07:15 -0600

    Using variables in the templates 2 way databinding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({ // esto es el decorator
     selector: 'courses', //como quiero que se llame mi nuevo elemento html
-    template: '<h2>Courses<h2>', //usualmente los templates contienen mas que una línea de código por lo tanto,
+    template: '<h2>{{ title }}<h2>', //usualmente los templates contienen mas que una línea de código por lo tanto,
     // en esta sección o puedo añadir el html puro o un path donde se encuentre el template. 
 })
-export class CoursesComponent {}
+export class CoursesComponent {
+    title = 'List of courses';
+
+    getTitle(){ //esta función también la puedo llamar dnd se esta llamando title en el
+        // template y obtendría el mismo resultado
+        return this.title;
+    }
+}
