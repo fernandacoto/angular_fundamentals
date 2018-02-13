fa99facf028030d953649f0b50133ca5c72450c3
Fernanda Coto <fermarioro@gmail.com>
2018-02-12 19:29:00 -0600

    Assignment Section 3 - End of section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,24 @@ import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component'; //sin la extensión del archivo
 import { CoursesService } from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent // Nuevo componente, recordar importarlo, como en la línea 7
+    CourseComponent,
+    AuthorsComponent // Nuevo componente, recordar importarlo, como en la línea 7
   ],
   imports: [
     BrowserModule
   ],
   providers: [
-    CoursesService // Esto necesita agregarse, para poder pasar la instancia como parametro en el constructor (dependency injection)
+    CoursesService, // Esto necesita agregarse, para poder pasar la instancia como parametro en el constructor (dependency injection)
     // cuando se registra algo aquí, angular va a crear un single instance
+    AuthorsService
   ],
   bootstrap: [AppComponent]
 })
